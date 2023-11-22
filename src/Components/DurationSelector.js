ab51681a7cf005a68ed4ba47893ef8f86c953b5c
Mahesh Kishore Vaswani <118044178+maheshv09@users.noreply.github.com>
2023-11-23 03:05:11 +0530

    Update DurationSelector.js

--- src/Components/DurationSelector.js
+++ src/Components/DurationSelector.js
@@ -5,6 +5,10 @@ const DurationSelector = ({
   durations,
   onSelectDuration,
   selectedDuration,
+  startDate, // Add startDate prop
+  endDate,   // Add endDate prop
+  onStartDateChange, // Add onStartDateChange prop
+  onEndDateChange,   // Add onEndDateChange prop
 }) => {
   return (
     <div className="duration-selector-container">
@@ -23,6 +27,26 @@ const DurationSelector = ({
           </option>
         ))}
       </select>
+
+      {/* Render start and end date inputs */}
+      {selectedDuration === "Weekly" && (
+        <div>
+          <label htmlFor="startDate">Start Date:</label>
+          <input
+            type="date"
+            id="startDate"
+            value={startDate}
+            onChange={(e) => onStartDateChange(e.target.value)}
+          />
+          <label htmlFor="endDate">End Date:</label>
+          <input
+            type="date"
+            id="endDate"
+            value={endDate}
+            onChange={(e) => onEndDateChange(e.target.value)}
+          />
+        </div>
+      )}
     </div>
   );
 };
