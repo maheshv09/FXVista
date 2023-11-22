7d715ebfb683a67b60a36738a848112085203145
Mahesh Kishore Vaswani <118044178+maheshv09@users.noreply.github.com>
2023-11-23 03:01:28 +0530

    Update CurrencyChart.js

--- src/Components/CurrencyChart.js
+++ src/Components/CurrencyChart.js
@@ -10,18 +10,64 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const CurrencyChart = ({ data, currencyName }) => {
+const CurrencyChart = ({
+  data,
+  currencyName,
+  startDate,
+  endDate,
+  selectedDuration,
+}) => {
+  const renderTooltip = (value) => [
+    value,
+    `Conversion Rate for 1 USD to ${currencyName}`,
+  ];
+
+  // Filter data based on selected duration, start date, and end date
+  const filteredData = data.filter((dataPoint) => {
+    const dataDate = new Date(dataPoint.label);
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+
+    switch (selectedDuration) {
+      case "Weekly":
+        const startOfWeek = new Date(startDate);
+        const endOfWeek = new Date(endDate);
+        return dataDate >= startOfWeek && dataDate <= endOfWeek;
+      case "Monthly":
+        return (
+          dataDate.getFullYear() * 100 + dataDate.getMonth() + 1 >=
+            startDateObj.getFullYear() * 100 + startDateObj.getMonth() + 1 &&
+          dataDate.getFullYear() * 100 + dataDate.getMonth() + 1 <=
+            endDateObj.getFullYear() * 100 + endDateObj.getMonth() + 1
+        );
+      case "Quarterly":
+        // Adjust this logic based on your specific definition of quarterly
+        const startQuarter =
+          Math.floor((startDateObj.getMonth() + 3) / 3) +
+          startDateObj.getFullYear() * 10;
+        const endQuarter =
+          Math.floor((endDateObj.getMonth() + 3) / 3) +
+          endDateObj.getFullYear() * 10;
+        const dataQuarter =
+          Math.floor((dataDate.getMonth() + 3) / 3) +
+          dataDate.getFullYear() * 10;
+        return dataQuarter >= startQuarter && dataQuarter <= endQuarter;
+      case "Yearly":
+        return (
+          dataDate.getFullYear() >= startDateObj.getFullYear() &&
+          dataDate.getFullYear() <= endDateObj.getFullYear()
+        );
+      default:
+        return true;
+    }
+  });
+
   return (
     <ResponsiveContainer width={700} height={212}>
-      <LineChart data={data}>
+      <LineChart data={filteredData}>
         <XAxis dataKey="label" />
         <YAxis />
-        <Tooltip
-          formatter={(value) => [
-            value,
-            `Conversion Rate for 1 USD to ${currencyName}`,
-          ]}
-        />
+        <Tooltip formatter={renderTooltip} />
         <Legend />
         <Line
           type="monotone"
